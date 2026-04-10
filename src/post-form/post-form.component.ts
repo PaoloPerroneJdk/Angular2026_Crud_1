b8da4cb92c215ca4d7168a7f373461b9f420b15f
PERRONE <paolo.perrone@capgemini.com>
2026-04-10 15:51:20 +0200

    Terzo commit

--- src/post-form/post-form.component.ts
+++ src/post-form/post-form.component.ts
@@ -1,18 +1,19 @@
 import { Component } from '@angular/core';
-import { FormGroup, Validators } from '@angular/forms';
+import { AbstractControl, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import { PostsService } from 'src/service/posts.service';
 import { FormBuilder,  ReactiveFormsModule } from '@angular/forms';
+import { Posts } from 'src/models/posts';
 @Component({
   selector: 'app-post-form',
   templateUrl: './post-form.component.html',
   styleUrls: ['./post-form.component.css']
 })
 export class PostFormComponent {
-
+  post: Posts [] =[]
   postForm ! : FormGroup;
     isEdit : boolean = false;
-
+submitted = false;
    constructor(private postService: PostsService, private route : ActivatedRoute, private formBuilder: FormBuilder, private router : Router) {
     
 this.postForm = this.formBuilder.group({
@@ -22,6 +23,41 @@ this.postForm = this.formBuilder.group({
     })
   }
   
-  onSubmit(){}
+  onSubmit(): void {
+  this.submitted = true;
+
+  if (this.postForm.invalid) {
+    return;
+  }
+
+  const formValue = this.postForm.value;
+
+  this.createNewPost(formValue);
+
+  console.log('Creato un nuovo post', formValue);
+}
+
+  createNewPost(post:Posts){
+    this.postService.createPost(this.postForm.value).subscribe({
+      next:() => {
+        this.router.navigate([''])
+      },
+      error:(error)=>{
+        console.log("Error")
+      }
+    })
+  }
+  
+  
+  
+get title() {
+  return this.postForm.get('title')!;
+}
+
+
+get description() {
+  return this.postForm.get('description')!;
+}
 
 }
+
