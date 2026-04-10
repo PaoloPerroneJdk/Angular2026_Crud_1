c9c99f7eeb975de9b71b506d4107f20f54234bc5
PERRONE <paolo.perrone@capgemini.com>
2026-04-10 11:31:21 +0200

    Second commit

--- src/post-form/post-form.component.ts
+++ src/post-form/post-form.component.ts
@@ -1,10 +1,27 @@
 import { Component } from '@angular/core';
-
+import { FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { PostsService } from 'src/service/posts.service';
+import { FormBuilder,  ReactiveFormsModule } from '@angular/forms';
 @Component({
   selector: 'app-post-form',
   templateUrl: './post-form.component.html',
   styleUrls: ['./post-form.component.css']
 })
 export class PostFormComponent {
+
+  postForm ! : FormGroup;
+    isEdit : boolean = false;
+
+   constructor(private postService: PostsService, private route : ActivatedRoute, private formBuilder: FormBuilder, private router : Router) {
+    
+this.postForm = this.formBuilder.group({
+    title: ['', Validators.required],
+    description: ['', Validators.required]
+
+    })
+  }
+  
+  onSubmit(){}
 
 }
